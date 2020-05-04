c9d18f0eed07c17c8edfcf2739c23fd6d38a7a4c
daraffleur <romaniuk.dana@gmail.com>
2020-05-04 22:44:04 +0300

    Connect Docker with Flask project

---
+++ docker/practice/app/models.py
@@ -0,0 +1,29 @@
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), nullable=False)
+    password = db.Column(db.String(), nullable=False)
+
+
+class Good(db.Model):
+    __tablename__ = 'goods'
+
+    good_id = db.Column(db.Integer, primary_key=True)
+    brand = db.Column(db.String(), nullable=False)
+    name = db.Column(db.String(), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+
+class Store(db.Model):
+    __tablename__ = 'stores'
+
+    store_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    city = db.Column(db.String(), nullable=False)
+    address = db.Column(db.String(), nullable=False)
+    manager_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
