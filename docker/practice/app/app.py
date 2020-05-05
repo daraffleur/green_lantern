c74b7e26848f605ee8b0c96d6ac33dc78f3eb605
daraffleur <48384633+daraffleur@users.noreply.github.com>
2020-05-05 14:06:46 +0300

    Update app.py

--- docker/practice/app/app.py
+++ docker/practice/app/app.py
@@ -7,41 +7,42 @@ from models import User, Good, Store
 
 db = SQLAlchemy()
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db.init_app(app)
+def get_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
 
-with app.app_context():
-    if database_exists(db.engine.url):
-        db.create_all()
-        print('Database exists')
-    else:
-        print("Database does not exists", {db.engine.url})
-        create_database(db.engine.url)  # create db
-        print('DB created')
+    with app.app_context():
+        if database_exists(db.engine.url):
+            db.create_all()
+            print('Database exists')
+        else:
+            print("Database does not exists", {db.engine.url})
+            create_database(db.engine.url)  # create db
+            print('DB created')
 
-with app.app_context():
-    users = get_users()
-    for user in users:
-        # print(user)
-        db.session.add(User(**user))
-    db.session.commit()
-    print("Data 'users' has already written in database")
+    with app.app_context():
+        users = get_users()
+        for user in users:
+            # print(user)
+            db.session.add(User(**user))
+        db.session.commit()
+        print("Data 'users' has already written in database")
 
-with app.app_context():
-    goods = get_goods()
-    for good in goods:
-        # print(good)
-        db.session.add(Good(**good))
-    db.session.commit()
-    print('Data "goods" has already written in database')
+    with app.app_context():
+        goods = get_goods()
+        for good in goods:
+            # print(good)
+            db.session.add(Good(**good))
+        db.session.commit()
+        print('Data "goods" has already written in database')
 
-with app.app_context():
-    stores = get_stores()
-    for store in stores:
-        #print(store)
-        db.session.add(Store(**store))
-    db.session.commit()
-    print('Data "stores" has already written in database')
+    with app.app_context():
+        stores = get_stores()
+        for store in stores:
+            #print(store)
+            db.session.add(Store(**store))
+        db.session.commit()
+        print('Data "stores" has already written in database')
 
 
