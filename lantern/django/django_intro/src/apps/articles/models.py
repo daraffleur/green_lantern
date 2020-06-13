219687356cc041f123f06d9cdf799f2e32c5f4a6
daraffleur <48384633+daraffleur@users.noreply.github.com>
2020-06-13 13:24:25 +0300

    Update models.py

--- lantern/django/django_intro/src/apps/articles/models.py
+++ lantern/django/django_intro/src/apps/articles/models.py
@@ -77,7 +77,7 @@ class Dish(models.Model):
 
 class Menu(models.Model):
     id = models.AutoField(primary_key=True)
-    id_of_dish = models.ForeignKey(Dish, db_column='id_of_dish', on_delete=models.DO_NOTHING, db_constraint=False)
+    dish_id = models.ForeignKey(Dish, db_column='dish_id', on_delete=models.DO_NOTHING, db_constraint=False)
 
     class Meta:
         db_table = 'menu'
@@ -85,29 +85,10 @@ class Menu(models.Model):
 
 
 class MenuOfRestaurant(models.Model):
-    id_of_menu = models.ForeignKey(Menu, db_column='id_of_menu', blank=True, null=False, on_delete=models.DO_NOTHING, db_constraint=False)
-    id_of_dish = models.ForeignKey(Dish, db_column='id_of_dish', blank=True, null=False, on_delete=models.DO_NOTHING, db_constraint=False)
+    menu_id = models.ForeignKey(Menu, db_column='menu_id', blank=True, null=False, on_delete=models.DO_NOTHING, db_constraint=False)
+    dish_id = models.ForeignKey(Dish, db_column='dish_id', blank=True, null=False, on_delete=models.DO_NOTHING, db_constraint=False)
 
     class Meta:
         db_table = 'menu_of_restaurant'
         managed = False
         verbose_name = "order"
-
-
-# class Tag(models.Model):
-#     name = models.CharField(max_length=40, unique=True)
-#
-#
-# class Article(models.Model):
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#
-#     title = models.CharField(max_length=255, verbose_name='Title', db_index=True)
-#     body = models.TextField(max_length=5000, verbose_name='Article body')
-#     tags = models.ManyToManyField(to='Tag', related_name='articles', blank=True)
-#     author = models.ForeignKey(
-#         to=settings.AUTH_USER_MODEL,
-#         null=True,
-#         on_delete=models.SET_NULL,
-#         related_name='articles'
-#     )
