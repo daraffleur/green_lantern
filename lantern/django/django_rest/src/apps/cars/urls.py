38697aebdda106738b07da04d37150ae0bf4ecf6
daraffleur <48384633+daraffleur@users.noreply.github.com>
2020-07-04 12:59:25 +0300

    Update urls.py

--- lantern/django/django_rest/src/apps/cars/urls.py
+++ lantern/django/django_rest/src/apps/cars/urls.py
@@ -11,8 +11,8 @@ router.register(r'', CarViewSet)
 
 urlpatterns = [
 
-                  path('car/', CarListView.as_view(), name="car of dealers"),
-                  path('car/created/', CarCreateView.as_view(), name='car creation'),
-                  path('car/<int:pk>/', CarUpdDelView.as_view(), name='car upd del')
+                  path('car/', CarListView.as_view(), name="car_of_dealers"),
+                  path('car/created/', CarCreateView.as_view(), name='car_creation'),
+                  path('car/<int:pk>/', CarUpdDelView.as_view(), name='car_upd_del')
 
               ] + router.urls
